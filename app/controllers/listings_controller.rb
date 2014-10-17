ac0c30dc6ff66ee05178a80082a7b75460f04d96
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-10-17 22:23:02 +0100

    Categories for Listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,7 +11,12 @@ class ListingsController < ApplicationController
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    if params[:category].blank?
+      @listings = Listing.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @listings = Listing.where(category_id: @category_id).order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    end      
   end
 
   # GET /listings/1
